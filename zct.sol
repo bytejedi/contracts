f52f41f0367d4fcbcf7b8de89d19f001bcd43f8e
bytejedi <me@bytejedi.com>
2020-07-15 18:02:58 +0800

    first commit

---
+++ zct.sol
@@ -0,0 +1,269 @@
+pragma solidity ^0.4.25;
+/**
+* Math operations with safety checks
+*/
+library SafeMath {
+    function safeMul(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a * b;
+        require(a == 0 || c / a == b);
+        return c;
+    }
+    function safeDiv(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0);
+        uint256 c = a / b;
+        require(a == b * c + a % b);
+        return c;
+    }
+    function safeSub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a);
+        return a - b;
+    }
+    function safeAdd(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c>=a && c>=b);
+        return c;
+    }
+}
+
+contract ZCT {
+    using SafeMath for uint256;
+    string public name = "ZCT";
+    string public symbol = "ZCT";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 0;
+    address public owner;
+    bool private _paused = false;
+
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceTokenOf; // accounts token balance
+    mapping (address => uint256) public freezeOf; // self freeze token balance
+    mapping (address => bool) public lockAccountOf; // contract owner lock account
+    mapping (address => uint256) public lockTokenOf; // contract owner lock account's token balance
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    /* This generates a public event on the blockchain that will notify clients
+    */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    /* This generates a public event on the blockchain that will notify clients
+    */
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    /* This notifies clients about the amount frozen */
+    event Freeze(address indexed from, uint256 value);
+    /* This notifies clients about the amount unfrozen */
+    event Unfreeze(address indexed from, uint256 value);
+
+    /* Initializes contract with initial supply tokens to the creator of the
+    contract */
+    constructor() public {
+        owner = msg.sender;
+    }
+    /* Send coins */
+    function transfer(address _to, uint256 _value) public returns (bool success)
+    {
+        require(!_paused);
+        // lock all transfer in critical situation
+        require(!lockAccountOf[msg.sender], "the sender is lock");
+        // need the sender is unlock
+        require(_to != 0x0);
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_value > 0);
+        // Check value
+        require(balanceTokenOf[msg.sender] >= _value);
+        // Check if the sender has enough
+        require(balanceTokenOf[_to] + _value >= balanceTokenOf[_to]);
+        // Check for overflows
+        require(balanceTokenOf[_to] + _value >= _value);
+        // Check for overflows
+        require(balanceTokenOf[msg.sender] >= lockTokenOf[msg.sender]);
+        // Check for lock
+        require(balanceTokenOf[msg.sender] >= lockTokenOf[msg.sender] + _value);
+        // Check for lock
+        balanceTokenOf[msg.sender] = SafeMath.safeSub(balanceTokenOf[msg.sender], _value);
+        // Subtract from the sender
+        balanceTokenOf[_to] = SafeMath.safeAdd(balanceTokenOf[_to], _value);
+        // Add the same to the recipient
+
+        emit Transfer(msg.sender, _to, _value);
+        // Notify anyone listening that this transfer took place
+        return true;
+    }
+    /* Allow another contract to spend some tokens in your behalf */
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        require((_value == 0) || (allowance[msg.sender][_spender] == 0));
+        // Only Reset, not allowed to modify
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    /* A contract attempts to get the coins */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(!_paused);
+        // lock all transfor in critical situation cases
+        require(!lockAccountOf[msg.sender], "the sender is lock");
+        // need the sender is unlock
+        require(_to != address(0));
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_value > 0);
+        // Check Value
+        require(balanceTokenOf[_from] >= _value);
+        // Check if the sender has enough
+        require(balanceTokenOf[_to] + _value > balanceTokenOf[_to]);
+        // Check for overflows
+        require(balanceTokenOf[_to] + _value > _value);
+        // Check for overflows
+        require(allowance[_from][msg.sender] >= _value);
+        // Check allowance
+        require(balanceTokenOf[_from] >= lockTokenOf[_from]);
+        // Check for lock
+        require(balanceTokenOf[_from] >= lockTokenOf[_from] + _value);
+        // Check for lock
+        balanceTokenOf[_from] = SafeMath.safeSub(balanceTokenOf[_from], _value);
+        // Subtract from the sender
+        balanceTokenOf[_to] = SafeMath.safeAdd(balanceTokenOf[_to], _value);
+        // Add the same to the recipient
+        allowance[_from][msg.sender] = SafeMath.safeSub(allowance[_from][msg.sender], _value);
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function freeze(uint256 _value) public returns (bool success) {
+        require(_value > 0);
+        require(balanceTokenOf[msg.sender] >= _value);
+        // Check if the sender has enough
+        require(freezeOf[msg.sender] + _value >= freezeOf[msg.sender]);
+        // Check for Overflows
+        require(freezeOf[msg.sender] + _value >= _value);
+        // Check for Overflows
+        balanceTokenOf[msg.sender] = SafeMath.safeSub(balanceTokenOf[msg.sender], _value);
+        // Subtract from the sender
+        freezeOf[msg.sender] = SafeMath.safeAdd(freezeOf[msg.sender], _value);
+        // Updates totalSupply
+        emit Freeze(msg.sender, _value);
+        return true;
+    }
+
+    function unfreeze(uint256 _value) public returns (bool success) {
+        require(_value > 0);
+        // Check Value
+        require(freezeOf[msg.sender] >= _value);
+        // Check if the sender has enough
+        require(balanceTokenOf[msg.sender] + _value > balanceTokenOf[msg.sender]);
+        // Check for Overflows
+        require(balanceTokenOf[msg.sender] + _value > _value);
+        // Check for overflows
+        freezeOf[msg.sender] = SafeMath.safeSub(freezeOf[msg.sender], _value);
+        // Subtract from the freeze
+        balanceTokenOf[msg.sender] = SafeMath.safeAdd(balanceTokenOf[msg.sender], _value);
+        // Add to balance
+        emit Unfreeze(msg.sender, _value);
+        return true;
+    }
+
+    function lockAccount(address _target) public returns (bool) {
+        require(msg.sender == owner, "the sender must be the owner");
+        lockAccountOf[_target] = true;
+        return true;
+    }
+
+    function unlockAccount(address _target) public returns (bool) {
+        require(msg.sender == owner, "the sender must be the owner");
+        lockAccountOf[_target] = false;
+        return true;
+    }
+
+    function batchMint(address[] receivers, uint256 amount) public  returns (bool) {
+        require(!_paused);
+        require(msg.sender == owner, "must the owner can mint");
+
+        uint arrayLength = receivers.length;
+
+        for(uint i=0; i<arrayLength; i++){
+            require(receivers[i] != address(0), "ERC20: mint to the zero address");
+
+            balanceTokenOf[receivers[i]] = balanceTokenOf[receivers[i]].safeAdd(amount);
+        }
+
+        totalSupply = totalSupply.safeAdd(amount.safeMul(arrayLength));
+
+        emit Transfer(address(0), receivers, amount);
+        return true;
+    }
+
+    function mint(address receiver, uint256 amount) public  returns (bool) {
+        require(!_paused);
+        require(msg.sender == owner, "must the owner can mint");
+        require(receiver != address(0), "ERC20: mint to the zero address");
+
+        balanceTokenOf[receiver] = balanceTokenOf[receiver].safeAdd(amount);
+
+        totalSupply = totalSupply.safeAdd(amount);
+
+        emit Transfer(address(0), receiver, amount);
+        return true;
+    }
+
+    function burn(uint256 _value) public returns (bool success) {
+        require(msg.sender == owner);
+        // Only Owner
+        require(_value > 0);
+        // Check Value
+        require(balanceTokenOf[msg.sender] >= _value);
+        // Check if the sender has enough
+        require(totalSupply >= _value);
+        // Check for overflows
+        balanceTokenOf[msg.sender] = SafeMath.safeSub(balanceTokenOf[msg.sender], _value);
+        // Subtract from the sender
+        totalSupply = SafeMath.safeSub(totalSupply, _value);
+        // Updates totalSupply
+        return true;
+    }
+
+    function lockToken(address _to, uint256 _value) public returns (bool success) {
+        require(msg.sender == owner);
+        // Only Owner
+        require(_to != 0x0);
+        // Prevent lock to 0x0 address
+        //require((_value == 0) || (lockTokenOf[_to] == 0));
+        // Only Reset, not allowed to modify
+        require(balanceTokenOf[_to] >= _value);
+        // Check for lock overflows
+        lockTokenOf[_to] = _value;
+        return true;
+    }
+
+    function unlockToken(address _to, uint256 _value) public returns (bool success) {
+        require(msg.sender == owner);
+        // Only Owner
+        require(_to != 0x0);
+        // Prevent lock to 0x0 address
+        //require((_value == 0) || (lockTokenOf[_to] == 0));
+        // Only Reset, not allowed to modify
+        require(lockTokenOf[_to] >= _value);
+        // Check for lock overflows
+        lockTokenOf[_to] = lockTokenOf[_to].safeSub(_value);
+        return true;
+    }
+
+    function pause() public returns (bool) {
+        require(msg.sender == owner);
+        // Only Owner
+        _paused = true;
+        return true;
+    }
+
+    function unpause() public returns (bool) {
+        require(msg.sender == owner);
+        // Only Owner
+        _paused = false;
+        return true;
+    }
+
+    function paused() public returns (bool) {
+        return _paused;
+    }
+
+    function () public payable {
+        revert();
+    }
+}
