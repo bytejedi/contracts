9def4a778a1d9341ab6b93b70b2c2122cc1ae273
bytejedi <me@bytejedi.com>
2020-07-16 14:46:12 +0800

    rename balanceTokenOf to balanceOf

--- zct.sol
+++ zct.sol
@@ -35,7 +35,7 @@ contract ZCT {
     bool public paused;
 
     /* This creates an array with all balances */
-    mapping (address => uint256) public balanceTokenOf; // accounts token balance
+    mapping (address => uint256) public balanceOf; // accounts token balance
     mapping (address => uint256) public freezeOf; // self freeze token balance
     mapping (address => bool) public lockAccountOf; // contract owner lock account
     mapping (address => uint256) public lockTokenOf; // contract owner lock account's token balance
@@ -71,19 +71,19 @@ contract ZCT {
         // Prevent transfer to 0x0 address. Use burn() instead
         require(_value > 0);
         // Check value
-        require(balanceTokenOf[msg.sender] >= _value);
+        require(balanceOf[msg.sender] >= _value);
         // Check if the sender has enough
-        require(balanceTokenOf[_to] + _value >= balanceTokenOf[_to]);
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
         // Check for overflows
-        require(balanceTokenOf[_to] + _value >= _value);
+        require(balanceOf[_to] + _value >= _value);
         // Check for overflows
-        require(balanceTokenOf[msg.sender] >= lockTokenOf[msg.sender]);
+        require(balanceOf[msg.sender] >= lockTokenOf[msg.sender]);
         // Check for lock
-        require(balanceTokenOf[msg.sender] >= lockTokenOf[msg.sender] + _value);
+        require(balanceOf[msg.sender] >= lockTokenOf[msg.sender] + _value);
         // Check for lock
-        balanceTokenOf[msg.sender] = SafeMath.safeSub(balanceTokenOf[msg.sender], _value);
+        balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);
         // Subtract from the sender
-        balanceTokenOf[_to] = SafeMath.safeAdd(balanceTokenOf[_to], _value);
+        balanceOf[_to] = SafeMath.safeAdd(balanceOf[_to], _value);
         // Add the same to the recipient
 
         emit Transfer(msg.sender, _to, _value);
@@ -94,18 +94,18 @@ contract ZCT {
     function batchTransfer(address[] _receivers, uint256[] _values, uint _len) public returns (bool) {
         require(!paused);
         require(!lockAccountOf[msg.sender], "the sender is lock");
-        require(balanceTokenOf[msg.sender] >= lockTokenOf[msg.sender]);
+        require(balanceOf[msg.sender] >= lockTokenOf[msg.sender]);
 
         for(uint i=0; i<_len; i++){
             require(_receivers[i] != address(0), "ERC20: mint to the zero address");
             require(_values[i] > 0);
-            require(balanceTokenOf[msg.sender] >= _values[i]);
-            require(balanceTokenOf[msg.sender] >= lockTokenOf[msg.sender] + _values[i]);
-            require(balanceTokenOf[_receivers[i]] + _values[i] >= balanceTokenOf[_receivers[i]]);
-            require(balanceTokenOf[_receivers[i]] + _values[i] >= _values[i]);
+            require(balanceOf[msg.sender] >= _values[i]);
+            require(balanceOf[msg.sender] >= lockTokenOf[msg.sender] + _values[i]);
+            require(balanceOf[_receivers[i]] + _values[i] >= balanceOf[_receivers[i]]);
+            require(balanceOf[_receivers[i]] + _values[i] >= _values[i]);
 
-            balanceTokenOf[msg.sender] = balanceTokenOf[msg.sender].safeSub(_values[i]);
-            balanceTokenOf[_receivers[i]] = balanceTokenOf[_receivers[i]].safeAdd(_values[i]);
+            balanceOf[msg.sender] = balanceOf[msg.sender].safeSub(_values[i]);
+            balanceOf[_receivers[i]] = balanceOf[_receivers[i]].safeAdd(_values[i]);
 
             emit Transfer(msg.sender, _receivers[i], _values[i]);
         }
@@ -130,21 +130,21 @@ contract ZCT {
         // Prevent transfer to 0x0 address. Use burn() instead
         require(_value > 0);
         // Check Value
-        require(balanceTokenOf[_from] >= _value);
+        require(balanceOf[_from] >= _value);
         // Check if the sender has enough
-        require(balanceTokenOf[_to] + _value > balanceTokenOf[_to]);
+        require(balanceOf[_to] + _value > balanceOf[_to]);
         // Check for overflows
-        require(balanceTokenOf[_to] + _value > _value);
+        require(balanceOf[_to] + _value > _value);
         // Check for overflows
         require(allowance[_from][msg.sender] >= _value);
         // Check allowance
-        require(balanceTokenOf[_from] >= lockTokenOf[_from]);
+        require(balanceOf[_from] >= lockTokenOf[_from]);
         // Check for lock
-        require(balanceTokenOf[_from] >= lockTokenOf[_from] + _value);
+        require(balanceOf[_from] >= lockTokenOf[_from] + _value);
         // Check for lock
-        balanceTokenOf[_from] = SafeMath.safeSub(balanceTokenOf[_from], _value);
+        balanceOf[_from] = SafeMath.safeSub(balanceOf[_from], _value);
         // Subtract from the sender
-        balanceTokenOf[_to] = SafeMath.safeAdd(balanceTokenOf[_to], _value);
+        balanceOf[_to] = SafeMath.safeAdd(balanceOf[_to], _value);
         // Add the same to the recipient
         allowance[_from][msg.sender] = SafeMath.safeSub(allowance[_from][msg.sender], _value);
 
@@ -154,13 +154,13 @@ contract ZCT {
 
     function freeze(uint256 _value) public returns (bool success) {
         require(_value > 0);
-        require(balanceTokenOf[msg.sender] >= _value);
+        require(balanceOf[msg.sender] >= _value);
         // Check if the sender has enough
         require(freezeOf[msg.sender] + _value >= freezeOf[msg.sender]);
         // Check for Overflows
         require(freezeOf[msg.sender] + _value >= _value);
         // Check for Overflows
-        balanceTokenOf[msg.sender] = SafeMath.safeSub(balanceTokenOf[msg.sender], _value);
+        balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);
         // Subtract from the sender
         freezeOf[msg.sender] = SafeMath.safeAdd(freezeOf[msg.sender], _value);
         // Updates totalSupply
@@ -173,13 +173,13 @@ contract ZCT {
         // Check Value
         require(freezeOf[msg.sender] >= _value);
         // Check if the sender has enough
-        require(balanceTokenOf[msg.sender] + _value > balanceTokenOf[msg.sender]);
+        require(balanceOf[msg.sender] + _value > balanceOf[msg.sender]);
         // Check for Overflows
-        require(balanceTokenOf[msg.sender] + _value > _value);
+        require(balanceOf[msg.sender] + _value > _value);
         // Check for overflows
         freezeOf[msg.sender] = SafeMath.safeSub(freezeOf[msg.sender], _value);
         // Subtract from the freeze
-        balanceTokenOf[msg.sender] = SafeMath.safeAdd(balanceTokenOf[msg.sender], _value);
+        balanceOf[msg.sender] = SafeMath.safeAdd(balanceOf[msg.sender], _value);
         // Add to balance
         emit Unfreeze(msg.sender, _value);
         return true;
@@ -202,7 +202,7 @@ contract ZCT {
         require(msg.sender == owner, "must the owner can mint");
         require(receiver != address(0), "ERC20: mint to the zero address");
 
-        balanceTokenOf[receiver] = balanceTokenOf[receiver].safeAdd(amount);
+        balanceOf[receiver] = balanceOf[receiver].safeAdd(amount);
 
         totalSupply = totalSupply.safeAdd(amount);
 
@@ -215,11 +215,11 @@ contract ZCT {
         // Only Owner
         require(_value > 0);
         // Check Value
-        require(balanceTokenOf[msg.sender] >= _value);
+        require(balanceOf[msg.sender] >= _value);
         // Check if the sender has enough
         require(totalSupply >= _value);
         // Check for overflows
-        balanceTokenOf[msg.sender] = SafeMath.safeSub(balanceTokenOf[msg.sender], _value);
+        balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);
         // Subtract from the sender
         totalSupply = SafeMath.safeSub(totalSupply, _value);
         // Updates totalSupply
@@ -233,7 +233,7 @@ contract ZCT {
         // Prevent lock to 0x0 address
         //require((_value == 0) || (lockTokenOf[_to] == 0));
         // Only Reset, not allowed to modify
-        require(balanceTokenOf[_to] >= _value);
+        require(balanceOf[_to] >= _value);
         // Check for lock overflows
         lockTokenOf[_to] = _value;
         return true;
